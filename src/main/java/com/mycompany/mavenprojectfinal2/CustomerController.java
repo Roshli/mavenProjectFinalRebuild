a9633fdded146bd1e2210d55b9d8a3d81f0089e3
Roshli <roshliperera@gmail.com>
2018-01-15 08:37:45 +0530

    saloon

--- src/main/java/com/mycompany/mavenprojectfinal2/CustomerController.java
+++ src/main/java/com/mycompany/mavenprojectfinal2/CustomerController.java
@@ -9,11 +9,7 @@ package com.mycompany.mavenprojectfinal2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -28,13 +24,15 @@ public class CustomerController {
     private Customer customer4 = new Customer();
     
     @RequestMapping(value = "/{tpNo}", method = RequestMethod.GET)
+    @CrossOrigin(origins = "http://loclhost:4200", allowedHeaders="*")
     public Customer viewCustomer(@PathVariable("tpNo") int tpNo)
     {
       customer4 = customerManage.viewCustomer(tpNo);
 
         return customer4;
     }
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://loclhost:4200", allowedHeaders="*")
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType. APPLICATION_JSON_VALUE)
     public void insertCustomer(@RequestBody Customer customer)
     {
         
